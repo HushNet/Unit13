e4b0379994d8252f322251fbf717a5ac066f819c
HushNet <pakhomov.alx@mail.ru>
2022-09-01 22:15:26 +0300

    123

---
+++ Unit13Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Diagnostics;
+using System.Threading.Channels;
+
+var str = File.ReadAllText("Text1.txt");
+
+string[] strings = str.Split();
+List<string> list = new List<string>();
+LinkedList<string> linkedList = new LinkedList<string>();
+
+Stopwatch listStop = new Stopwatch();
+
+listStop.Start();
+foreach (var s in strings)
+{
+    linkedList.AddLast(s);
+}
+listStop.Stop();
+Console.WriteLine(listStop.ElapsedMilliseconds);
+
+Stopwatch linkedListStop = new Stopwatch();
+linkedListStop.Start();
+foreach (var s in strings)
+{
+    list.Add(s);
+}
+linkedListStop.Stop();
+Console.WriteLine(linkedListStop.ElapsedMilliseconds);
---
+++ Unit13Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+var str = File.ReadAllText("Text1.txt");
+
+str = str.Replace(',', ' ');
+str = str.Replace('.', ' ');
+
+string[] strings = str.Trim().Split();
+
+
+Dictionary<string, int> repeats = new Dictionary<string, int>();
+int maxRepeats = 0;
+foreach (var s in strings)
+{
+    if (repeats.ContainsKey(s))
+    {
+        repeats[s]++;
+
+        if (repeats[s] > maxRepeats)
+            maxRepeats = repeats[s];
+    }
+    else
+    {
+        repeats.Add(s,1);
+    }
+}
+repeats.OrderBy(x => x.Value);
+
+var keys = repeats.Keys.ToList();
+for (int i = 0; i < 11; i++)
+{
+    Console.WriteLine(keys[i]);
+}
